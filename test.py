6125c5cc15b53216b0f9b07804496bd558dd5f6a
vegas <vegas@inl.ics.keio.ac.jp>
2017-10-25 12:28:45 +0900

    remote test

---
+++ test.py
@@ -0,0 +1,33 @@
+import sys
+import ConfigParser
+from neo4j.v1 import GraphDatabase, basic_auth
+
+## read config file
+config = ConfigParser.SafeConfigParser()
+try:
+    config.read('neo.conf')
+    ip_asama = config.get('asama', 'ip')
+    port_asama = config.get('asama', 'port')
+except:
+    print "Error occured in reading config"
+    exit()
+ 
+print ("asama ip: %s" %ip_asama)
+print ("asama port: %s" %port_asama)
+
+bolt = "bolt://"
+bolt_asama = bolt + ip_asama
+print("bolt_asama: %s" %bolt_asama)
+
+driver = GraphDatabase.driver(bolt_asama, auth=basic_auth("neo4j", "neo4jadmin"))
+session = driver.session()
+
+v_name = 'Nakamura'
+
+result = session.run("MATCH (a:Person { name : '%s'})-[r:ACTED_IN]->(b) "
+"RETURN a.name,type(r) AS Type, b.title" % (v_name))
+
+for record in result:
+    print("%s-%s->%s " % (record["a.name"], record["Type"], record["b.title"]))
+
+session.close()
