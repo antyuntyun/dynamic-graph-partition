ad8d1af8dff76462b82e4d695baa3104b7f879b4
vegas <vegas@aap.asama.ce.net>
2017-10-30 03:23:32 -0400

    add module

--- test.py
+++ test.py
@@ -25,11 +25,23 @@ driver = GraphDatabase.driver(bolt_asama, auth=basic_auth("neo4j", "neo4jadmin")
 session = driver.session()
 
 v_name = 'Nakamura'
-
 result = session.run("MATCH (a:Person { name : '%s'})-[r:ACTED_IN]->(b) "
 "RETURN a.name,type(r) AS Type, b.title" % (v_name))
 
 for record in result:
     print("%s-%s->%s " % (record["a.name"], record["Type"], record["b.title"]))
+
+def add_friends(tx, name, friend_name):
+    tx.run("MERGE (a:Person {name: $name}) "
+           "MERGE (a)-[:KNOWS]->(friend:Person {name: $friend_name})",
+           name=name, friend_name=friend_name)
+
+def print_friends(tx, name):
+    for record in tx.run("MATCH (a:Person)-[:KNOWS]->(friend) WHERE a.name = $name "
+                         "RETURN friend.name ORDER BY friend.name", name=name):
+        print(record["friend.name"])
+
+
+
 
 session.close()
