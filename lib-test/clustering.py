15d3ea78efda77e7b013745fedbb9de845865139
vegas <vegas@inl.ics.keio.ac.jp>
2017-11-08 12:43:47 +0900

    add GraphModule/Clusterinng.py

---
+++ lib-test/clustering.py
@@ -0,0 +1,15 @@
+import networkx as nx
+import community
+
+def louvainclusteringplot(G):
+  partition = community.best_partition(G)                                                                                                           
+  size = float(len(set(partition.values())))                                                                                                 
+  pos = nx.spring_layout(G)                                                                                                                  
+  count = 0.                                                                                                                                 
+  for com in set(partition.values()):                                                                                                        
+    count += 1.                                                                                                                              
+    list_nodes = [nodes for nodes in partition.keys() if partition[nodes] == com]                                                            
+    nx.draw_networkx_nodes(G, pos, list_nodes, node_size=150, node_color = str(count/size))                                                                                                                                 
+  plt.show()
+
+
