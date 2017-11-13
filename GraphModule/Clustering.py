233d0ead49f5629776d0e96c7a870275edccedc7
vegas <vegas@inl.ics.keio.ac.jp>
2017-11-13 16:57:39 +0900

    modify /GraphModule/Clustering.py

--- GraphModule/Clustering.py
+++ GraphModule/Clustering.py
@@ -3,14 +3,15 @@ import community
 import matplotlib.pyplot as plt
 
 def louvainclusteringplot(G):
-  partition = community.best_partition(G)                                                                                                           
-  size = float(len(set(partition.values())))                                                                                                 
-  pos = nx.spring_layout(G)                                                                                                                  
-  count = 0.                                                                                                                                 
-  for com in set(partition.values()):                                                                                                        
-    count += 1.                                                                                                                              
-    list_nodes = [nodes for nodes in partition.keys() if partition[nodes] == com]                                                            
-    nx.draw_networkx_nodes(G, pos, list_nodes, node_size=150, node_color = str(count/size))                                                                                                                                 
+  partition = community.best_partition(G) 
+  size = float(len(set(partition.values())))
+  pos = nx.spring_layout(G)
+  count = 0.
+  for com in set(partition.values()):
+    count += 1.
+    list_nodes = [nodes for nodes in partition.keys() if partition[nodes] == com]
+    nx.draw_networkx_nodes(G, pos, list_nodes, node_size=150, node_color = str(count/size))
   plt.show()
+
 
 
