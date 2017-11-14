2b2097136aa3429f4f494b4efe6638cbc64acc6c
vegas <vegas@inl.ics.keio.ac.jp>
2017-11-14 16:47:34 -0500

    add /dataset/get-TL.py /GraphModule/SampleGraph.py

---
+++ GraphModule/SampleGraph.py
@@ -0,0 +1,10 @@
+import networkx as nx
+from numpy.random import *
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
---
+++ dataset/get-TL.py
@@ -0,0 +1,41 @@
+# coding:utf-8
+
+from requests_oauthlib import  OAuth1Session
+import json, datetime, time
+import sys, ConfigParser
+import pandas as pd
+
+#read config file
+config = ConfigParser.SafeConfigParser()
+try:
+    config.read('twitter.conf')
+    CK = config.get('twitter', 'CK')
+    CS = config.get('twitter', 'CS')
+    AT = config.get('twitter', 'AT')
+    AS = config.get('twitter', 'AS')   
+except:
+    print "Error occured in reading config"
+    exit()
+print ("CK: " + CK)
+print ("CS: " + CS)
+print ("AT: " + AT)
+print ("AS: " + AS)
+session = OAuth1Session(CK, CS, AT, AS)
+ 
+url = 'https://api.twitter.com/1.1/statuses/home_timeline.json'
+params = {'count': 100}
+
+TweetList = []
+
+for i in range(100):
+    req = session.get(url, params = params)
+    if req.status_code == 200:
+        timeline = json.loads(req.text)
+        for tweet in timeline:
+            TweetList.append(tweet["text"])
+    else:
+        print ("Error: %d" % req.status_code)
+    time.sleep(240)
+
+df = pd.DataFrame(TweetList)
+df.to_csv('TweetList.csv')
